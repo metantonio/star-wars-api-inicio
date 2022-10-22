922ea61becd5117cee2c8533fd294697420600a8
metantonio <antonio_martinez88@hotmail.com>
2022-10-22 02:19:30 +0000

    upd

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Favorite_People
 #from models import Person
 
 app = Flask(__name__)
@@ -94,6 +94,14 @@ def delete_user_by_id(user_id):
     db.session.delete(user)
     db.session.commit()
     return jsonify("usuario eliminado exitósamente"), 200
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    peoples = People.query.all()
+    #print(users)
+    peoples = list(map( lambda people: people.serialize(), peoples)) 
+    #print(users)  
+    return jsonify(peoples), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -7,8 +7,10 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(250), unique=False, nullable=False) 
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    description = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
+    user_favorite = db.relationship("Favorite_People", backref="user")
 
+    #de manera automática, el nombre de la tabla es el nombre de la clase en minúscula
     def __repr__(self):
         return '<User %r>' % self.email
 
@@ -19,4 +21,30 @@ class User(db.Model):
             "is_active": self.is_active,
             "descripcion": self.description
             # do not serialize the password, its a security breach
+        }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    mass = db.Column(db.Float)
+    people_favorite = db.relationship("Favorite_People", backref="people")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "mass": self.mass          
+        }
+
+class Favorite_People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #con el nombre de la tabla user y atributo id
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    #Esta es una tabla pivote para relacionar User y People, relación muchos a muchos
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_email": User.query.get(self.user_id).serialize()['email'],
+            "character_name": People.query.get(self.people_id).serialize()['name']          
         }
